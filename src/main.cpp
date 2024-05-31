1e3b55bfeb812a9eda9546b9cc0e45d0326a4b5a
Reem Hoisman <reemho@std.hac.ac.il>
2024-06-01 01:54:49 +0300

    added player sprite sheets

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
  * Re'em Hoisman - 211694146 
  *---------------------------------------------**/
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	auto w = sf::RenderWindow(sf::VideoMode(1280, 1280), "game");
 	return 0;
 }
