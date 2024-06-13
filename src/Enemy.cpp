042820f2521aa9d4c1b5acc1561d10cd54cd1d78
Reem Hoisman <reemho@std.hac.ac.il>
2024-06-13 08:56:32 +0300

    ...

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,4 +1,4 @@
 #include "Enemy.h"
 
 Enemy::Enemy(sf::Sprite& sprite, std::unique_ptr<MovingStrategy> strategy, Animation& animation):
-	Entity(sprite), m_moveStrategy(std::move(strategy)), m_animation(animation){}
+	Entity(sprite), m_moveStrategy(std::move(strategy)), m_animation(animation){}//
