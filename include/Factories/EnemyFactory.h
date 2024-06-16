d21f38774be657b82883a58a12349f477a7a07ed
alon zargari <alonza@edu.hac.ac.il>
2024-06-16 22:51:05 +0300

    ....

--- include/Factories/EnemyFactory.h
+++ include/Factories/EnemyFactory.h
@@ -10,8 +10,8 @@ using enemyMap = std::map<ObjectAnimation, createFunc>;
 class EnemyFactory
 {
 public:
-	static std::unique_ptr<Enemy>createSideToSideEnemy(const ObjectAnimation, sf::Sprite&);
 	static std::unique_ptr<Enemy>createJumpingEnemy(const ObjectAnimation, sf::Sprite&);
+	static std::unique_ptr<Enemy>createSideToSideEnemy(const ObjectAnimation, sf::Sprite&);
 	static bool registerEnemy(const ObjectAnimation, createFunc);
 
 private:
--- src/States/GameState/InGameState.cpp
+++ src/States/GameState/InGameState.cpp
@@ -56,11 +56,11 @@ void InGameState::initTileMap()
 				sprite = createNewObjectSprite(factor_x, 500, "basicEnemiesSheet");
 				m_entities.emplace_back(EnemyFactory::createSideToSideEnemy(PEPPER_ENEMY, sprite));
 			}
-			/*else if (image.getPixel(x, y) == sf::Color(255, 127, 39))
+			else if (image.getPixel(x, y) == sf::Color(255, 127, 39))
 			{
 				sprite = createNewObjectSprite(factor_x, 500, "basicEnemiesSheet");
-				m_entities.emplace_back(EnemyFactory::createSideToSideEnemy(ORANGE_ENEMY, sprite));
-			}*/
+				m_entities.emplace_back(EnemyFactory::createJumpingEnemy(ORANGE_ENEMY, sprite));
+			}
 			factor_x += 85.f; 
 		}
 		factor_y -= 90.f; //the height of each texture
