70a0d44813deddffb8221ae586b2726eec44010d
Reem Hoisman <reemho@std.hac.ac.il>
2024-06-16 22:58:18 +0300

    ...

--- src/Strategies/WalkAndJumpStrategy.cpp
+++ src/Strategies/WalkAndJumpStrategy.cpp
@@ -4,15 +4,16 @@ WalkAndJumpStrategy::WalkAndJumpStrategy(): m_isJumping(false){}
 
 sf::Vector2f& WalkAndJumpStrategy::move(sf::Time deltaTime, bool rightDirection, float gravity)
 {
-	m_walkTimer -= deltaTime;
 	sf::Vector2f newPos;
 	float speed;
 	gravity == 0 ? speed = deltaTime.asSeconds() * 180.f : speed = 0;
 	
 	if (m_isJumping)
 		newPos.y = -deltaTime.asSeconds() * 600.f + gravity;
-	else
+	else {
 		newPos.y = gravity;
+	}
+
 
 	if (rightDirection)
 	{
