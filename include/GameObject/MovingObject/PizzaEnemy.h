68b53a1f8dde7dd1b8d1e5944344af167c181737
alon zargari <alonza@edu.hac.ac.il>
2024-06-19 17:56:40 +0300

    .....

--- include/GameObject/MovingObject/PizzaEnemy.h
+++ include/GameObject/MovingObject/PizzaEnemy.h
@@ -1,18 +1,18 @@
-//#pragma once
-//#include "GameObject/MovingObject/Enemy.h"
-//#include "DesignPatterns/States/PizzaEnemyStates/PizzaEnemyState.h"
-//
-//class PizzaEnemy: public Enemy
-//{
-//public:
-//    PizzaEnemy(sf::Sprite& sprite, std::unique_ptr<MovingStrategy> strategy, Animation& animation);
-//    virtual void move(sf::Time) override;
-//    void increaseJumps();
-//    void movePizza(sf::Time);
-//
-//private:
-//    static bool m_register;
-//    float m_WalkTimer;
-//    int m_jumps;
-//    std::unique_ptr< PizzaEnemyState> m_state;
-//};
+#pragma once
+#include "GameObject/MovingObject/Enemy.h"
+#include "DesignPatterns/States/PizzaEnemyStates/PizzaEnemyState.h"
+
+class PizzaEnemy: public Enemy
+{
+public:
+    PizzaEnemy(sf::Sprite& sprite, std::unique_ptr<MovingStrategy> strategy);
+    virtual void move(sf::Time) override;
+    void increaseJumps();
+    void movePizza(sf::Time);
+
+private:
+    static bool m_register;
+    float m_WalkTimer;
+    int m_jumps;
+    std::unique_ptr< PizzaEnemyState> m_state;
+};
