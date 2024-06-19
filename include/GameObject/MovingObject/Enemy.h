158f2270957ead670ee8312951db1721ca033afa
Reem Hoisman <reemho@std.hac.ac.il>
2024-06-19 11:12:46 +0300

    ...

--- include/GameObject/MovingObject/Enemy.h
+++ include/GameObject/MovingObject/Enemy.h
@@ -11,18 +11,11 @@ class Enemy : public Entity
 {
 public:
 	Enemy(sf::Sprite&, std::unique_ptr<MovingStrategy>, Animation&);
-	void loadStrategy(std::unique_ptr<MovingStrategy> strategy);
-	void loadAnimation(const ObjectAnimation&);
-	void loadNewFrame(sf::Time);
 	std::unique_ptr<MovingStrategy> getStrategy()const;
 
 protected:
-	
+	void setStrategy(std::unique_ptr<MovingStrategy>);
 
 private:
-	Animation m_animation;
 	std::unique_ptr<MovingStrategy> m_moveStrategy;
-	
-	int m_animationIndex;
-	sf::Time m_elapsed;
-};
+}
