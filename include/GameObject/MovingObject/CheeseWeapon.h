8803452188b4ceaa0ed4af82c93438d75a0b121e
alon zargari <alonza@edu.hac.ac.il>
2024-06-19 20:26:23 +0300

    ....

--- include/GameObject/MovingObject/CheeseWeapon.h
+++ include/GameObject/MovingObject/CheeseWeapon.h
@@ -3,5 +3,9 @@
 
 class CheeseWeapon :public Weapon 
 {
-
+public:
+	CheeseWeapon(sf::Sprite&,std::unique_ptr<MovingStrategy>,bool);
+	virtual void move(sf::Time)override;
+private:
+	bool m_rightDirection;
 };
